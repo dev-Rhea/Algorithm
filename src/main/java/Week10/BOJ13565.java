bfb9536178ca24e8b087e764362bebde8d3e5c67
rhea <dev.rhea4444@gmail.com>
2024-06-18 21:17:26 +0900

    변경

--- src/main/java/Week10/BOJ13565.java
+++ src/main/java/Week10/BOJ13565.java
@@ -1,2 +1,57 @@
-package Week10;public class BOJ13565 {
+package Week10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ13565 {
+    static int N, M;
+    static int[][] map; // 격자
+    static boolean[][] visit; // 방문 체크
+    static boolean check = false; // 도착 했을 때 전기흐르면 true로 바꿔줌
+    static int[][] dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}; // 방향
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visit = new boolean[N][M];
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine(); // 격자 각 줄 문자열로 읽고, 문자를 숫자로 변환하여 배열에 넣어줌
+            for(int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j) - '0'; // 도착 했을 때 1인지 아닌지 판단
+            }
+        }
+
+        for(int i=0;i<M;i++) {
+            if(map[0][i] == 0) {
+                DFS(0,i); // 바깥 라인 값 0 이면 DFS 돌려줌
+            }
+        }
+        System.out.println(check ? "YES" : "NO");
+
+    }
+
+    public static void DFS(int x, int y) {
+        visit[x][y] = true;
+        if(x == N - 1 && map[x][y] == 0){ // 가장 내부에 전기가 흐른다면 true 출력되도록
+            check = true;
+            return;
+        }
+
+        for(int i = 0;i<4;i++) { // 4 방향 탐색하면서 이동
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+
+            // 범위 안에 있고, 방문하지 않았고, 전기가 흐르지 않은 곳이면 DFS 돌려줌
+            if(nx >= 0 && ny >= 0 && nx < N && ny < M && !visit[nx][ny] && map[nx][ny] == 0) {
+                DFS(nx, ny); // 재귀
+            }
+        }
+    }
 }
--- src/main/java/Week7/BOJ1753.java
+++ src/main/java/Week7/BOJ1753.java
@@ -1,2 +1,78 @@
-package Week7;public class BOJ1753 {
+package Week7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ1753 {
+    static int V, E, K;
+    static List<List<Node>> graph;
+    static boolean[] visit;
+    static int[] dijkstra;
+
+    static class Node implements Comparable<Node> {
+        int node;
+        int value;
+
+        Node(int node, int value){
+            this.node = node;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.value, o.value);
+        }
+    }
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>(V + 1);
+        for(int i = 0; i <= V; i++) graph.add(new ArrayList<>());
+        for(int i = 0; i < E; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph.get(u).add(new Node(v, w));
+        }
+        dijkstra(K);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= V; i++){
+            if(dijkstra[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
+            else sb.append(dijkstra[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dijkstra(int start){
+        visit = new boolean[V + 1];
+        dijkstra = new int[V + 1];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        Arrays.fill(dijkstra, Integer.MAX_VALUE);
+        dijkstra[start] = 0;
+        pq.add(new Node(start, 0));
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            if(visit[cur.node]) continue;
+            visit[cur.node] = true;
+
+            for(Node n : graph.get(cur.node)){
+                if(!visit[n.node] && dijkstra[n.node] > dijkstra[cur.node] + n.value){
+                    dijkstra[n.node] = dijkstra[cur.node] + n.value;
+                    pq.add(new Node(n.node, dijkstra[n.node]));
+                }
+            }
+        }
+    }
 }
