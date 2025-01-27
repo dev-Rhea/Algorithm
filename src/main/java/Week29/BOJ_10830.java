2ccc91634ae3326466d06056126f4dad93aa4819
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-27 20:13:33 +0900

    :sparkles: study

---
+++ src/main/java/Week29/BOJ_10830.java
@@ -0,0 +1,65 @@
+package Week29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10830 {
+
+    static int N;
+    static long B;
+    static int[][] matrix;
+    static int[][] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        matrix = new int[N][N];
+        result = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+            result[i][i] = 1;
+        }
+
+        while (B > 0) {
+            if (B % 2 == 1) {
+                result = multiply(result, matrix);
+            }
+            matrix = multiply(matrix, matrix);
+            B /= 2;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(result[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int[][] multiply(int[][] a, int[][] b) {
+        int[][] temp = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    temp[i][j] += a[i][k] * b[k][j];
+                    temp[i][j] %= 1000;
+                }
+            }
+        }
+        return temp;
+    }
+
+}
