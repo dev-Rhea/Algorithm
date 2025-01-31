2863dc3d846db9fad07808e9d584b57fff5bba67
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-31 15:51:29 +0900

    :sparkles: study

---
+++ src/main/java/Week29/BOJ_14002.java
@@ -0,0 +1,51 @@
+package Week29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_14002 {
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        dp = new int[N + 1];
+
+        // 공백으로 구분된 여러 개의 숫자 입력 처리
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); // 정수 하나씩 저장
+        }
+
+        dp[0] = 1;
+        int max = dp[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = N - 1; i >= 0; i--) {
+            if (dp[i] == max) {
+                stack.push(arr[i]);
+                max--;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
