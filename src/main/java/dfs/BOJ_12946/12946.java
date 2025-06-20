b1a048620453b1a57f622d5e6920c0f2176be2d6
dev-Rhea <dev.rhea4444@gmail.com>
2025-06-20 16:26:16 +0900

    study

--- src/main/java/dfs/BOJ_12946/12946.java
+++ src/main/java/dfs/BOJ_12946/12946.java
@@ -1,3 +1,4 @@
+package dfs.BOJ_12946;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/main/java/dfs/BOJ_16947/16947.java
+++ src/main/java/dfs/BOJ_16947/16947.java
@@ -1,3 +1,4 @@
+package dfs.BOJ_16947;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
