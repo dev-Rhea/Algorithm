acbf67500bf1efead72f01cccf1b4276211252ff
dev-Rhea <dev.rhea4444@gmail.com>
2025-08-01 08:02:25 +0900

    study

--- src/main/java/DP/BOJ_1309/1309.java
+++ src/main/java/DP/BOJ_1309/1309.java
@@ -8,17 +8,14 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        if (N == 1) {
-            System.out.println(3);
-            return;
-        }
         
         int[] dp = new int[N+1];
         dp[1] = 3;
-        dp[2] = 7;
+        if (N >= 2) {
+            dp[2] = 7;
+        }
 
-        for(int i=3; i<=N;i++){
+        for(int i = 3; i <= N; i++){
             dp[i] = (dp[i-2] + dp[i-1] * 2) % 9901;
         }
 
