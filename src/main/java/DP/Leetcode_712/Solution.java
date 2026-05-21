994702070f40a9f7711d50a33510fc41dbea9e96
dev-Rhea <dev.rhea4444@gmail.com>
2026-05-21 16:26:47 +0900

    :sparkles: study

--- src/main/java/DP/Leetcode_712/Solution.java
+++ src/main/java/DP/Leetcode_712/Solution.java
@@ -8,10 +8,10 @@ public class Solution {
 
         int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
         }
-        for (int j = 1; j < m; j++) {
+        for (int j = 1; j <= m; j++) {
             dp[0][j] = dp[0][j - 1] + s2.charAt(j - 1);
         }
 
