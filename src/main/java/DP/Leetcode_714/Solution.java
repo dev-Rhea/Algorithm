dc1853d7fb12c517c27d5ac6998d1fad27f3124e
dev-Rhea <dev.rhea4444@gmail.com>
2026-06-22 19:38:18 +0900

    :sparkles: study

---
+++ src/main/java/DP/Leetcode_714/Solution.java
@@ -0,0 +1,16 @@
+package DP.Leetcode_714;
+
+public class Solution {
+
+    public int maxProfit(int[] prices, int fee) {
+        int cash = 0;
+        int hold = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+
+        return cash;
+    }
+}
